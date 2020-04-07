5a18a1a7225fda267c9a704fa3e77fff0df213ea
Namtrik Development <namtrik@namtrikdev.co>
2020-04-07 17:10:19 -0500

    Agrega el trabajo con vistas basadas en funciones, enrutamiento y plantillas.

--- geekside/blog/views.py
+++ geekside/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+# vista basada en función
+def first_view(request):
+  # logica...
+  # return HttpResponse("<h1>No vives de ensalada ##</h1>")
+  contexto = {
+    'food': 'carne'
+  }
+  return render(request, 'blog/my_template.html', context=contexto)
+
+def all_posts(request):
+  contexto = {
+    'posts': Post.objects.all(),
+    'recent_posts': Post.objects.all().order_by('-created_at')[:2]
+  }
+  return render(request, 'blog/posts.html', context=contexto)
+
+# vistas basadas en clases
