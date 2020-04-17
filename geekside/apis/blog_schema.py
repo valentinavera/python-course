108e24aef13131be6b7ac6015a34f217b1345f99
Valentina <valentinav@unicauca.edu.co>
2020-04-16 20:49:49 -0500

    api

---
+++ geekside/apis/blog_schema.py
@@ -0,0 +1,34 @@
+from graphene_django import DjangoObjectType
+from blog.models import Category as CategoryModel, Post as PostModel
+from graphene import relay, ObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+from graphene_django.forms.mutation import DjangoModelFormMutation
+
+class Category(DjangoObjectType):
+    #nodo del grafo
+    class Meta:
+        model = CategoryModel
+        filter_fields = {
+            'name':['exact', 'icontains', 'istartswith'],
+            'is_active':['exact']
+        }
+        interfaces = (relay.Node, )
+
+class Post (DjangoObjectType):
+    class Meta: 
+        model = PostModel
+        filter_fields = {
+            'title': ['exact', 'icontains', 'istartswith'],
+            'text':['icontains'],
+            'excerpt': ['icontains'],
+            'is_active':['exact'],
+            'category__name':['exact','icontains'],
+            'created_at':['gt', 'gte', 'lt', 'lte']
+        }
+        interfaces = (relay.Node, )
+
+class Query(ObjectType):
+    categories = DjangoFilterConnectionField(Category)
+    category = relay.Node.Field(Category)
+    posts = DjangoFilterConnectionField(Post)
+    post = relay.Node.Field(Post)
--- geekside/apis/schema.py
+++ geekside/apis/schema.py
@@ -1,9 +1,11 @@
 from graphene import ObjectType, String, Schema
+from .blog_schema import Query as BlogQuery
 
-class Query(ObjectType):
-    hello = String(name=String(default_value="stranger"))
+class Query(BlogQuery):
+    pass
+    # hello = String(name=String(default_value="stranger"))
 
-    def resolve_hello(root, info, name, *args, **kwargs):
-        return 'Hello %s' % name
+    # def resolve_hello(root, info, name, *args, **kwargs):
+    #     return 'Hello %s' % name
 
 ROOT_SCHEMA = Schema(query=Query)
